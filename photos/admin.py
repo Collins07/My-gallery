719c8d2d6d7499d386bdcbc0e0fc12f1f72015ed
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-29 15:54:46 +0300

    register the models

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,10 @@
+from atexit import register
 from django.contrib import admin
-
+from . models import Photo, Category
 # Register your models here.
+
+
+admin.site.register(Category)
+admin.site.register(Photo)
+
+
