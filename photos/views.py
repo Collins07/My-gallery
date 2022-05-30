599078d2a5b6a2f039d092cb0086da7c1caac012
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-30 12:28:50 +0300

    set up the database

--- photos/views.py
+++ photos/views.py
@@ -6,8 +6,14 @@ from .models import Category, Photo
 # Create your views here.
 
 def gallery(request):
+    category = request.GET.get('category')
+    
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)    
+
     categories = Category.objects.all()
-    photos = Photo.objects.all()
 
 
     context = {'categories':categories, 'photos':photos}
@@ -24,4 +30,29 @@ def viewPhoto(request):
 
 
 def addPhoto(request):
-    return render (request, 'photos/add.html')        
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=['category_new'])
+        else:
+            category = None
+
+        photo = Photo.objects.create(
+            category = category,
+            description = data['description'],
+            image = image,
+        )    
+
+        return redirect ('gallery')   
+        
+
+
+    context = {'categories':categories}
+
+    return render (request, 'photos/add.html', context)        
